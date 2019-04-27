6e63df021d24c2a02a8271377ea993942e2cdfe8
goodzlmn <goodzlmn@naver.com>
2019-04-27 22:24:12 +0900

    Day_4

---
+++ SortPrograming/boj10989.java
@@ -0,0 +1,29 @@
+package SortPrograming;
+
+import java.util.*;
+import java.io.*;
+import java.sql.Array;
+
+public class boj10989 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int cnt[] = new int[10001];
+		
+		for(int i=0; i<N; i++) {
+			cnt[Integer.parseInt(br.readLine())]++;
+		}
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		for(int i=1; i<=10000; i++) {
+			if(cnt[i] > 0) {
+				for(int j=0; j<cnt[i]; j++) {
+					bw.write(Integer.toString(i) + "\n");
+				}
+			}
+		}
+		br.close();
+		bw.close();
+	}
+}
---
+++ SortPrograming/boj11652.java
@@ -0,0 +1,33 @@
+package SortPrograming;
+
+import java.util.*;
+import java.io.*;
+
+public class boj11652 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		long num[] = new long[N];
+		
+		for(int i=0; i<N; i++) {
+			num[i] = Long.parseLong(br.readLine());
+		}
+		
+		long ans = num[0];
+		int cnt = 1;
+		int ans_cnt = 1;
+		
+		for(int i=1; i<N; i++) {
+			if(num[i] == num[i-1]) {
+				cnt += 1;
+			} else {
+				cnt = 1;
+			}
+			if(ans_cnt < cnt) {
+				ans_cnt = cnt;
+				ans = num[i];
+			}
+		}
+		System.out.println(ans);
+	}
+}
